742ef25aa22e5761f4267df267693df6bf658cab
NeedCookies <emilkalim895@gmail.com>
2024-06-17 20:25:20 +0300

    Add some properties into Block, Course entities

--- Domain/Entities/Course.cs
+++ Domain/Entities/Course.cs
@@ -8,8 +8,11 @@ namespace Domain.Entities
         public int Id { get ; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public decimal Price { get; set; }
         public string UserId { get; set; }
         public IdentityUser User { get; set; }
         public string ImgPath { get; set; }
+        public ICollection<Lesson> Lessons { get; set; }
+        public ICollection<Quiz> Quizes { get; set; }
     }
 }
