296c12fb2ed9e8bc007b0e925dfc7cfbb08638a7
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-06-19 23:26:24 +0300

    Создал миграции

--- Domain/Entities/Identity/AppRole.cs
+++ Domain/Entities/Identity/AppRole.cs
@@ -4,6 +4,8 @@ namespace Domain.Entities.Identity
 {
     public class AppRole : IdentityRole<string>
     {
+        public AppRole() { }
+
         public AppRole(string roleName)
         {
             Name = roleName;
