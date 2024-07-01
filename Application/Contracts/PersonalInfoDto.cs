4d266e50d93cc7ac5d254fda23ecf2c3acc1e9bc
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-07-01 19:29:20 +0300

    Добавил DTO личной информации

---
+++ Application/Contracts/PersonalInfoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Contracts
+{
+    public class PersonalInfoDto
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? PictureUrl { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public int Points { get; set; }
+        public string Role { get; set; } = null!;
+    }
+}
