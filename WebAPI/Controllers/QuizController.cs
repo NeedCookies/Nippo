359198220ba731642909b296362d684abf3da5fc
NeedCookies <emilkalim895@gmail.com>
2024-07-01 09:40:40 +0300

    Change quiz controller methods names

--- WebAPI/Controllers/QuizController.cs
+++ WebAPI/Controllers/QuizController.cs
@@ -6,33 +6,33 @@ namespace WebAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class QuizController(IQuizService quizService): ControllerBase
+    public class QuizController(IQuizService quizService) : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetQuizesByCourse(int courseId)
+        public async Task<IActionResult> GetByCourse(int courseId)
         {
             var quizes = await quizService.GetByCourseId(courseId);
             return Ok(quizes);
         }
 
         [HttpGet("get-quiz")]
-        public async Task<IActionResult> GetQuizById(int quizId)
+        public async Task<IActionResult> GetById(int quizId)
         {
             var quiz = await quizService.GetById(quizId);
             return Ok(quiz);
         }
 
         [HttpPost("create")]
-        public async Task<IActionResult> CreateQuiz([FromBody] CreateQuizRequest request)
+        public async Task<IActionResult> Create([FromBody] CreateQuizRequest request)
         {
-            var quiz = quizService.Create(request);
+            var quiz = await quizService.Create(request);
             return Ok(quiz);
         }
 
-        [HttpDelete]
-        public async Task<IActionResult> DeleteBook(int quizId)
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(int quizId)
         {
-            var quiz = quizService.Delete(quizId);
+            var quiz = await quizService.Delete(quizId);
             return Ok(quiz);
         }
     }
