d3de4985670b12840408da13515612f98c0cf1b5
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-07-03 15:14:52 +0300

    Добавил получение личной информации на бэкенд

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Abstractions.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController (IUserService userService): ControllerBase
+    {
+        [Authorize]
+        [HttpGet("get-personal-info")]
+        public async Task<IActionResult> GetUserInfo()
+        {
+            var userId = GetUserId();
+            var userInfo = await userService.GetUserInfoById(userId);
+
+            return Ok(userInfo);
+        }
+
+        private string GetUserId()
+        {
+            return HttpContext.User.FindFirst("userId")!.Value;
+        }
+    }
+}
