d6c4a853e2b7ca3a5d0e792f60de6146ee3d54d7
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-07-04 16:11:48 +0300

    Добавил новые DTO

---
+++ Application/Contracts/GivePointsRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Contracts
+{
+    public class GivePointsRequest
+    {
+        public string UserId { get; set; } = null!;
+        public int Points { get; set; }
+    }
+}
---
+++ Application/Contracts/UserInfoUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Contracts
+{
+    public class UserInfoUpdateRequest
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? BirthDate { get; set; }
+    }
+}
