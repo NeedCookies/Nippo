af4c32c8a2617131382ce76bf695b2b828db981a
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-07-04 16:13:41 +0300

    Добавил обработку запросов с фронта на бэкенде

---
+++ WebAPI/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Abstractions.Services;
+using Application.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("admin")]
+    [Authorize(Roles = "admin")]
+    public class AdminController(IUserService userService): ControllerBase
+    {
+        [HttpPost("give-points")]
+        public async Task<IActionResult> GivePointsToUser(GivePointsRequest givePointsRequest)
+        {
+            var newUserInfo = await userService.GivePointsToUser(givePointsRequest.UserId, givePointsRequest.Points);
+
+            return Ok(newUserInfo);
+        }
+    }
+}
--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -1,14 +1,15 @@
 ﻿using Application.Abstractions.Services;
+using Application.Contracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("user")]
     public class UserController (IUserService userService): ControllerBase
     {
-        [Authorize]
         [HttpGet("get-personal-info")]
         public async Task<IActionResult> GetUserInfo()
         {
@@ -16,6 +17,18 @@ namespace WebAPI.Controllers
             var userInfo = await userService.GetUserInfoById(userId);
 
             return Ok(userInfo);
+        }
+
+        [HttpPost("update-personal-info")]
+        public async Task<IActionResult> UpdateUserInfo(
+            [FromForm] UserInfoUpdateRequest infoUpdateRequest,
+            IFormFile? userPicture)
+        {
+            var userId = GetUserId();
+
+            var newUserInfo = await userService.UpdateUserInfo(userId, infoUpdateRequest, null);
+
+            return Ok(newUserInfo);
         }
 
         private string GetUserId()
