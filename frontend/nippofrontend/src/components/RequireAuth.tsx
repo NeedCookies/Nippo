aedb75b8eb3aabf6d46dd3f03ebdd61bf3d9e5b6
NeedCookies <emilkalim895@gmail.com>
2024-07-08 20:09:52 +0300

    Hide navbar from register and login pages, fix linking to loginpage when user not loginned

--- frontend/nippofrontend/src/components/RequireAuth.tsx
+++ frontend/nippofrontend/src/components/RequireAuth.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { LoginPage } from "../pages/LoginPage";
 import { useNavigate } from "react-router-dom";
 
@@ -12,9 +12,16 @@ const useLoginStatus = () => {
 const RequireAuth: FC<{ children: React.ReactElement }> = ({ children }) => {
   const userIsLogged = useLoginStatus();
   const navigate = useNavigate();
+  useEffect(() => {
+    if (!userIsLogged) {
+      navigate("/login");
+    }
+  }, [userIsLogged, navigate]);
+
   if (!userIsLogged) {
-    return <LoginPage />;
+    return null; // Возвращаем null, чтобы ничего не рендерить до тех пор, пока не произойдет перенаправление
   }
+
   return children;
 };
 
