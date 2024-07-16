d46c93588c601166bbc3700714b6549bd267ef2b
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-07-17 02:43:18 +0300

    Спрятал некоторые порты, настроил прокси

--- frontend/nippofrontend/src/axios/axiosDefaults.ts
+++ frontend/nippofrontend/src/axios/axiosDefaults.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "http://localhost/api";
 
 axios.interceptors.request.use(function (config) {
         config.withCredentials = true; 
