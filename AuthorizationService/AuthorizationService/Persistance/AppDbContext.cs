d4d4782e33ecf40926abf32a68d4aa0375a9e4f6
Emil Kalimullin <emilkalim895@gmail.com>
2024-11-14 12:38:43 +0300

    Add Db Context to Persistance layer

---
+++ AuthorizationService/AuthorizationService/Persistance/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using AuthorizationService.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Options;
+
+namespace AuthorizationService.Persistance
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+        
+
+
+        #region Required
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.Id).IsRequired();
+            modelBuilder.Entity<User>()
+                .Property(u => u.FirstName).IsRequired().HasMaxLength(256);
+            modelBuilder.Entity<User>()
+                .Property(u => u.LastName).IsRequired().HasMaxLength(256);
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email).IsRequired().HasMaxLength(256);
+            modelBuilder.Entity<User>()
+                .Property(u => u.PasswordHash).IsRequired().HasMaxLength(1024);
+            modelBuilder.Entity<User>()
+                .Property(u => u.BirthDate).IsRequired();
+        }
+        #endregion
+    }
+}
--- AuthorizationService/AuthorizationService/Program.cs
+++ AuthorizationService/AuthorizationService/Program.cs
@@ -1,3 +1,6 @@
+using AuthorizationService.Persistance;
+using Microsoft.EntityFrameworkCore;
+
 namespace AuthorizationService
 {
     public class Program
@@ -5,6 +8,14 @@ namespace AuthorizationService
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
+
+            builder.Services.AddDbContext<AppDbContext>(
+                options =>
+                {
+                    options.UseNpgsql(configuration.GetConnectionString(nameof(AppDbContext)));
+                });
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
